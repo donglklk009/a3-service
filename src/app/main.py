da51bb79f058b3f1dbfca4f1426db79df48ad363
khoidd <khoidd@outlook.com>
2021-08-26 19:07:21 +0700

    update code

--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,3 @@
-import time
-
-
-def xprint(*values: object):
-    print(*values, flush=True)
-
-
-def main():
-    xprint("Start")
-    time.sleep(10)
-    xprint("Stop")
-
 
 if __name__ == "__main__":
-    main()
+    print("hello")
